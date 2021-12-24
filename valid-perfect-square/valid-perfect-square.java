70a3f8cf6eebf2c0dbd963119d69edf1fe1e425e
Vansh Gupta <gvansh.411@gmail.com>
2021-12-24 21:55:12 +0530

    Time: 997 ms (5.01%), Space: 35.5 MB (94.52%) - LeetHub

--- valid-perfect-square/valid-perfect-square.java
+++ valid-perfect-square/valid-perfect-square.java
@@ -1,12 +1,14 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-        int i;
-        int rem = num%10;
+         int rem = num%10;
         if(rem == 2 || rem == 3 || rem == 7 || rem == 8){   //Any number which ends with either 2, 3, 7, 8 will not be a perfect square.
              return false;  
         }
-        for(i=1; i*i<num; i++){
+        for(int i=1;i<=num;i++){
+            if(i*i==num){
+                return true;
+            }
         }
-            return (i*i == num);
+        return false;
     }
 }
