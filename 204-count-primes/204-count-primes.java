0679c5bf1db711c8db33cd62cd460cbb2c3166a3
Vansh Gupta <gvansh.411@gmail.com>
2022-01-13 16:40:36 +0530

    Time: 142 ms (46.90%), Space: 65.4 MB (14.78%) - LeetHub

---
+++ 204-count-primes/204-count-primes.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countPrimes(int n) {
+        boolean prime[] = new boolean[n + 1];
+        for (int i = 0; i <= n; i++)
+            prime[i] = true;
+
+        for (int p = 2; p * p <= n; p++) {
+            // If prime[p] is not changed, then it is a prime
+            if (prime[p] == true) {
+                // Update all multiples of p
+                for (int i = p * p; i <= n; i += p)
+                    prime[i] = false;
+            }
+        }
+        int count=0;
+        for (int i = 2; i < n; i++){   // Print all prime numbers
+            if (prime[i] == true)
+                count++;
+        }
+        return count;
+    }
+}
