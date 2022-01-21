a3a107e7fa5c4ef5a96ff16c5b1d5e7b0ff3a6fe
Vansh Gupta <gvansh.411@gmail.com>
2022-01-22 00:04:29 +0530

    Time: 2 ms (41.89%), Space: 41.9 MB (7.73%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.java
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -1,35 +1,15 @@
 class Solution {
     public int maxProduct(int[] arr) {
-        int maxLeft = arr[0];
-	    int maxRight = arr[0];
-	    
-	    boolean zeroPresent = false;
-	    
-	    int prod = 1;
-	    
-	    for(int i:arr) {
-	        prod *= i;
-	        if(i == 0) {
-	            zeroPresent = true;
-	            prod = 1;
-	            continue;
-	        }
-	        maxLeft = Math.max(maxLeft,prod);
-	    }
-	    
-	    prod = 1;
-	    
-	    for(int j=arr.length-1;j>=0;j--) {
-	        prod *= arr[j];
-	        if(arr[j] == 0) {
-	            zeroPresent = true;
-	            prod = 1;
-	            continue;
-	        }
-	        maxRight = Math.max(maxRight,prod);
-	    }
-	    
-	    if(zeroPresent) return Math.max(Math.max(maxLeft,maxRight),0);
-	    return Math.max(maxLeft,maxRight);
+        int minprod = arr[0],maxprod = arr[0],result = arr[0],choice1,choice2;
+    
+    for(int i=1;i<arr.length;i++) {
+        choice1=minprod*arr[i];
+        choice2=maxprod*arr[i];
+        minprod = Math.min(arr[i],Math.min(choice1,choice2));
+        maxprod = Math.max(arr[i],Math.max(choice1,choice2));
+        
+        result = Math.max(result,maxprod);
+    }
+    return result;
     }
 }
