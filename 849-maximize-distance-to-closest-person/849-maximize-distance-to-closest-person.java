a02e7e1b17656bc4403791e671428fa628371875
Vansh Gupta <gvansh.411@gmail.com>
2022-01-27 04:31:43 +0530

    Time: 3 ms (66.46%), Space: 46.5 MB (36.54%) - LeetHub

--- 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.java
+++ 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.java
@@ -1,55 +1,23 @@
 class Solution {
     public int maxDistToClosest(int[] seats) {
-//         BRUTE FORCE
         int n=seats.length;
-        int[] leftD=new int[n];
-        int[] rightD=new int [n];
-        int left=1;
-        int right=1;
-        int last=-1;
-        int first=-1;
-        boolean flag=true;
-        
-        //leftDist array banaya
+        int recentOne=-1;    
+        int maxDist=0;
         for(int i=0;i<n;i++){
             if(seats[i]==1){
-               leftD[i]=0; 
-                left=1;
-                last=i;
-                if(flag==true){
-                    first=i;
-                    flag=false;
+                if(recentOne==-1){   //i.e. Case 1
+                    maxDist=i;
+                }else{    //ie Case 2
+                    maxDist=Math.max(maxDist,(i-recentOne)/2);
                 }
-            }else{
-                leftD[i]=left;
-                left++;
+                recentOne=i;
             }
         }
         
-        //rightDist array banaya
-        for(int i=n-1;i>=0;i--){
-            if(seats[i]==1){
-               rightD[i]=0; 
-                right=1;
-            }else{
-                rightD[i]=right;
-                right++;
-            }
-        }
-        //ek aur baar traverse karke max nikala
-        int max=0;
-        for(int i=0;i<n;i++){
-          max=Math.max(max,Math.min(leftD[i],rightD[i]));
+        if(seats[n-1]==0){   //i.e. Vase 3
+            maxDist=Math.max(maxDist,n-recentOne-1);
         }
         
-        if(seats[n-1]==0){   //last mai 0 wala case handled
-            max=Math.max(max,n-last-1);
-        }
-        
-         if(seats[0]==0){   //starting mai 0 wala case handled
-            max=Math.max(max,first);
-        }
-        
-        return max;
+        return maxDist;
     }
 }
