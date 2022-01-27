5ad88b86bceeab277092ed173da0fc26102e88cb
Vansh Gupta <gvansh.411@gmail.com>
2022-01-27 18:27:31 +0530

    Time: 3 ms (58.03%), Space: 63.9 MB (5.28%) - LeetHub

--- 1375-number-of-times-binary-string-is-prefix-aligned/1375-number-of-times-binary-string-is-prefix-aligned.java
+++ 1375-number-of-times-binary-string-is-prefix-aligned/1375-number-of-times-binary-string-is-prefix-aligned.java
@@ -1,12 +1,13 @@
 class Solution {
     public int numTimesAllBlue(int[] light) {
-        int rightMost=-1;
+        int rightMostBulbThatIsON=-1;
         int count=0;
         for(int i=0;i<light.length;i++){
-            if(light[i]>rightMost){
-                rightMost=light[i];
+            //i+1 is no. of bulbs that you turned ON
+            if(light[i]>rightMostBulbThatIsON){
+                rightMostBulbThatIsON=light[i];
             }
-            if(rightMost==i+1){
+            if(rightMostBulbThatIsON==i+1){
                 count++;
             }
         }
