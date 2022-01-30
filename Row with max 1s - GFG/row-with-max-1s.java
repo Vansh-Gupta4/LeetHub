6367d957f3221cba1896b98e2f637d551c1784e5
Vansh Gupta <gvansh.411@gmail.com>
2022-01-30 22:33:00 +0530

    Added solution - LeetHub

--- Row with max 1s - GFG/row-with-max-1s.java
+++ Row with max 1s - GFG/row-with-max-1s.java
@@ -38,18 +38,30 @@ class Solution {
         int Maxcount=0;
         int row=-1;
         for(int i=0;i<n;i++){
-            int rowCount=0;
-            for(int j=0;j<m;j++){
-                if(arr[i][j]==1){
-                    rowCount++;
-                }
-
-                if(rowCount>Maxcount){
-                    Maxcount=rowCount;
-                    row=i;
-                }
+            int countOf1inEachRow=binarySearch(arr,i);
+            
+            if(countOf1inEachRow > Maxcount){
+                Maxcount=countOf1inEachRow;
+                row=i;
             }
         }
         return row;
+    }
+    public static int binarySearch(int[][] mat,int r){
+        int lo=0;
+        int hi=mat[0].length-1;
+        int FirstIndexOf1=mat[0].length;
+        
+        while(lo<=hi){
+            int mid = (lo+hi)/2;
+            if(mat[r][mid]==1){
+                FirstIndexOf1=mid;
+                hi=mid-1;
+            }else{
+                lo=mid+1;
+            }
+        }
+        int count=mat[0].length-FirstIndexOf1;
+        return count;
     }
 }
