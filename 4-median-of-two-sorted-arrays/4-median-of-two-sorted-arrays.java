34776daddb60486532b213eea022dd5f67a4b887
Vansh Gupta <gvansh.411@gmail.com>
2022-02-02 04:18:09 +0530

    Time: 4 ms (58.22%), Space: 49.8 MB (6.65%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
@@ -1,28 +1,33 @@
 class Solution {
     public double findMedianSortedArrays(int[] arr1, int[] arr2) {
-        int m=arr1.length;
-        int n=arr2.length; 
-        int finalArray[]=new int[n+m];
-        int i=0,j=0,k=0;
-        while(i<m && j<n) {
-            if(arr1[i]<arr2[j]) {
-                finalArray[k++] = arr1[i++];
-            }else {
-                finalArray[k++] = arr2[j++];
-            }
+         int m = arr1.length;
+         int n = arr2.length;
+         if(m>n){
+            return findMedianSortedArrays(arr2,arr1);//ensuring that binary search happens on minimum size array
+         }
+        
+         int low=0,high=m,medianPos=((m+n)+1)/2;
+        while(low<=high) {
+            int cut1 = (low+high)/2;
+            int cut2 = medianPos - cut1;
+        
+            int l1 = (cut1 == 0)? Integer.MIN_VALUE:arr1[cut1-1];
+            int l2 = (cut2 == 0)? Integer.MIN_VALUE:arr2[cut2-1];
+            int r1 = (cut1 == m)? Integer.MAX_VALUE:arr1[cut1];
+            int r2 = (cut2 == n)? Integer.MAX_VALUE:arr2[cut2];
+        
+            if(l1<=r2 && l2<=r1) {
+                if((m+n)%2 != 0){   //length of merged arr will be even
+                    return Math.max(l1,l2);
+                }else{   //will be odd
+                    return (Math.max(l1,l2)+Math.min(r1,r2))/2.0;
+                } 
+            }else if(l1>r2) {
+                high = cut1-1;
+            }else{
+                low = cut1+1;
+            } 
         }
-        if(i<m) {
-            while(i<m) 
-                finalArray[k++] = arr1[i++];
-        }
-        if(j<n) {
-            while(j<n){
-                 finalArray[k++] = arr2[j++];
-            }
-        }
-        n = n+m;
-        if(n%2==1) 
-            return finalArray[((n+1)/2)-1];
-        else return ((double)finalArray[(n/2)-1]+(double)finalArray[(n/2)])/2.0;
+        return 0.0;
     }
 }
