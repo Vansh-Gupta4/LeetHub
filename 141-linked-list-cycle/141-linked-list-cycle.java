d3b6748a229736049be469c506499c476cd9e4d1
Vansh Gupta <gvansh.411@gmail.com>
2022-02-15 00:35:08 +0530

    Time: 0 ms (100.00%), Space: 45.7 MB (17.62%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -10,23 +10,16 @@
  * }
  */
 public class Solution {
-    public boolean hasCycle(ListNode h) {
-        HashSet<ListNode> s = new HashSet<ListNode>();
-        while (h != null) {
-            // If we have already has this node
-            // in hashmap it means their is a cycle
-            // (Because you we encountering the
-            // node second time).
-            if (s.contains(h))
-                return true;
- 
-            // If we are seeing the node for
-            // the first time, insert it in hash
-            s.add(h);
- 
-            h = h.next;
+    public boolean hasCycle(ListNode head) {
+        if(head == null) return false;
+        ListNode fast = head;
+        ListNode slow = head;
+        
+        while(fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow) return true;
         }
- 
-        return false;
+    return false;
     }
 }
