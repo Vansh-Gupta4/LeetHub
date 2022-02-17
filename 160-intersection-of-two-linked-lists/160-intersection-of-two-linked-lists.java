9d9bbbf925cd4354995c1d735d5698606dd9a0d6
Vansh Gupta <gvansh.411@gmail.com>
2022-02-18 00:22:14 +0530

    Time: 1 ms (98.83%), Space: 45.1 MB (69.23%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -11,15 +11,31 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode head1, ListNode head2) {
-    HashSet<ListNode> st=new HashSet<>();
-    while(head1 != null) {
-       st.add(head1);
-       head1 = head1.next;
+        int diff = getDifference(head1,head2);
+        if(diff < 0){
+            while(diff++ != 0) head2 = head2.next; 
+        } 
+        else{
+          while(diff-- != 0) head1 = head1.next;  
+        } 
+        while(head1 != null) {
+            if(head1 == head2) return head1;
+            head2 = head2.next;
+            head1 = head1.next;
+        }
+        return head1;
     }
-    while(head2 != null) {
-        if(st.contains(head2)) return head2;
-        head2 = head2.next;
-    }
-    return null;
+     static int getDifference(ListNode head1,ListNode head2) {
+     int len1 = 0,len2 = 0;
+        while(head1 != null || head2 != null) {
+            if(head1 != null) {
+                ++len1; head1 = head1.next;
+            }
+            if(head2 != null) {
+                ++len2; head2 = head2.next;
+            }
+            
+        }
+        return len1-len2;//if difference is neg-> length of list2 > length of list1 else vice-versa
     }
 }
