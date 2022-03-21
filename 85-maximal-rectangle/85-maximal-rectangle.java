16daeb27e87d7a48cddaf449b26ab738fc3feaf4
Vansh Gupta <gvansh.411@gmail.com>
2022-03-21 18:58:20 +0530

    Time: 40 ms (45.60%), Space: 55.6 MB (11.69%) - LeetHub

---
+++ 85-maximal-rectangle/85-maximal-rectangle.java
@@ -0,0 +1,39 @@
+class Solution {
+   public int maximalRectangle(char[][] matrix) {
+        if(matrix.length==0) return 0;
+        // for each cell with value=1, we look upward (north), the number of continuous '1' is the height of cell
+        int[] heights = new int[matrix[0].length];
+        int maxArea=-1;
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                if(matrix[i][j]=='0'){
+                    heights[j] = 0;
+                } else {
+                    heights[j] ++;
+                }
+            }            
+            int area = largestRectangleArea(heights);
+            maxArea = Math.max(maxArea, area);
+        }
+        return maxArea;
+    }
+    public int largestRectangleArea(int[] heights) {
+       Stack < Integer > st = new Stack < > ();
+        int maxA = 0;
+        int n = heights.length;
+        for (int i = 0; i <= n; i++) {
+            while (!st.empty() && (i == n || heights[st.peek()] >= heights[i])) {
+                int height = heights[st.peek()];
+                st.pop();
+                int width;
+                if (st.empty())
+                    width = i;
+                else
+                    width = i - st.peek() - 1;
+                maxA = Math.max(maxA, width * height);
+            }
+            st.push(i);
+        }
+        return maxA;
+    }
+}
