03d2d0ea31446941c2199f6ad569cc61c8b0b42d
Vansh Gupta <gvansh.411@gmail.com>
2022-04-20 19:41:55 +0530

    Time: 2 ms (57.84%), Space: 40.8 MB (79.16%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.java
+++ 844-backspace-string-compare/844-backspace-string-compare.java
@@ -1,22 +1,17 @@
 class Solution {
-    public boolean backspaceCompare(String s, String t) {
-         Stack<Character> st1 = new Stack();
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)!='#'){
-                st1.push(s.charAt(i));
-            }else if(!st1.isEmpty()){
-                st1.pop();
+ public boolean backspaceCompare(String S, String T) {
+        return compute(S).equals(compute(T));
+    }
+    
+    private Stack<Character> compute(String S) {
+        Stack<Character> stack = new Stack();
+        for (char c : S.toCharArray()) {
+            if (c != '#') {
+                stack.push(c);
+            } else if (!stack.isEmpty()) {
+                stack.pop();
             }
         }
-        Stack<Character> st2 = new Stack();
-        for(int i=0;i<t.length();i++){
-            if(t.charAt(i)!='#'){
-                st2.push(t.charAt(i));
-            }else if(!st2.isEmpty()){
-                st2.pop();
-            }
-        }
-        System.out.println(String.valueOf(st1)+"+++++"+String.valueOf(st2));
-        return String.valueOf(st1).equals(String.valueOf(st2));
+        return stack;
     }
 }
