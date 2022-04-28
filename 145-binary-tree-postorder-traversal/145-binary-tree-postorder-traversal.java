77f8db4fc5fea8d116178fc5cba50976e83fc6c6
Vansh Gupta <gvansh.411@gmail.com>
2022-04-28 13:04:48 +0530

    Time: 1 ms (42.12%), Space: 41.9 MB (63.55%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
@@ -14,16 +14,45 @@
  * }
  */
 class Solution {
-    public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> ret = new ArrayList<>();
-        dfs(root,ret);
-        return ret;
-    }
-    public void dfs(TreeNode root, List<Integer> ret) {
-         if(root!=null){
-            dfs(root.left,ret);
-            dfs(root.right,ret);
-            ret.add(root.val);
-        }
-    }
+    //RECURSIVE Solution
+    // public List<Integer> postorderTraversal(TreeNode root) {
+    //     List<Integer> ret = new ArrayList<>();
+    //     dfs(root,ret);
+    //     return ret;
+    // }
+    // public void dfs(TreeNode root, List<Integer> ret) {
+    //      if(root!=null){
+    //         dfs(root.left,ret);
+    //         dfs(root.right,ret);
+    //         ret.add(root.val);
+    //     }
+    // }
+    
+//     ITERATIVE SOLUTION
+     public List<Integer> postorderTraversal(TreeNode root) {
+        ArrayList < Integer > postOrder = new ArrayList < > ();
+        if (root == null){
+          return postOrder;  
+        } 
+
+        Stack < TreeNode > s1 = new Stack < > ();
+        Stack < TreeNode > s2 = new Stack < > ();
+        s1.push(root);
+         while(!s1.isEmpty()){
+             TreeNode top=s1.peek();
+             s1.pop();
+             s2.push(top);
+             if(top.left!=null){
+                 s1.push(top.left);
+             }
+            if(top.right!=null){
+                 s1.push(top.right);
+             }
+         }
+          while (!s2.isEmpty()) {
+            postOrder.add(s2.peek().val);
+            s2.pop();
+          }
+         return postOrder;
+     }
 }
