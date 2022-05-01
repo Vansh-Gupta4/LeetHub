d66d6f14ebf9f5e6e9c1954e794d24e3b2b35749
Vansh Gupta <gvansh.411@gmail.com>
2022-05-01 15:11:30 +0530

    Time: 4 ms (67.47%), Space: 42.9 MB (57.38%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.java
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.java
@@ -26,41 +26,74 @@ class Tuple {
     }
 } 
 //     Using Inorder Traversal
-    public List<List<Integer>> verticalTraversal(TreeNode root) {
-        TreeMap < Integer, TreeMap < Integer, PriorityQueue < Integer >>> map = new TreeMap < > ();
-        Queue < Tuple > q = new LinkedList < Tuple > ();
-        q.offer(new Tuple(root, 0, 0));
-        while (!q.isEmpty()) {   //for entire level order traversal
-            Tuple tuple = q.poll();
-            TreeNode node = tuple.node;
-            int x = tuple.row;
-            int y = tuple.col;
+//     public List<List<Integer>> verticalTraversal(TreeNode root) {
+//         TreeMap < Integer, TreeMap < Integer, PriorityQueue < Integer >>> map = new TreeMap < > ();
+//         Queue < Tuple > q = new LinkedList < Tuple > ();
+//         q.offer(new Tuple(root, 0, 0));
+//         while (!q.isEmpty()) {   //for entire level order traversal
+//             Tuple tuple = q.poll();
+//             TreeNode node = tuple.node;
+//             int x = tuple.row;
+//             int y = tuple.col;
 
 
-            if (!map.containsKey(x)) { //if for vertical ...treeMap doesn't exist...then make it
-                map.put(x, new TreeMap < > ());
-            }
-            if (!map.get(x).containsKey(y)) {//if for level ...PriorityQueue doesn't exist...then make it
-                map.get(x).put(y, new PriorityQueue < > ());
-            }
-            map.get(x).get(y).offer(node.val); //PriorityQ mai insert kardi val
+//             if (!map.containsKey(x)) { //if for vertical ...treeMap doesn't exist...then make it
+//                 map.put(x, new TreeMap < > ());
+//             }
+//             if (!map.get(x).containsKey(y)) {//if for level ...PriorityQueue doesn't exist...then make it
+//                 map.get(x).put(y, new PriorityQueue < > ());
+//             }
+//             map.get(x).get(y).offer(node.val); //PriorityQ mai insert kardi val
 
-            if (node.left != null) {  //left exist karta h toh left jaaoo
-                q.offer(new Tuple(node.left, x - 1, y + 1));
+//             if (node.left != null) {  //left exist karta h toh left jaaoo
+//                 q.offer(new Tuple(node.left, x - 1, y + 1));
+//             }
+//             if (node.right != null) { //right exist karta h toh right jaaoo
+//                 q.offer(new Tuple(node.right, x + 1, y + 1));
+//             }
+//         }
+//         List < List < Integer >> list = new ArrayList < > ();
+//         for (TreeMap < Integer, PriorityQueue < Integer >> ys: map.values()) { //sab vertical par iterate
+//             list.add(new ArrayList < > ());   //empty list banai sab vertical ke liye
+//             for (PriorityQueue < Integer > nodes: ys.values()) {
+//                 while (!nodes.isEmpty()) {
+//                     list.get(list.size() - 1).add(nodes.poll());
+//                 }
+//             }
+//         }
+//         return list;
+//     }
+//}    
+    
+//     USING PREORDER TRAVERSAL
+     public List<List<Integer>> verticalTraversal(TreeNode root) {
+        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
+
+        solve(root, map, 0, 0);
+
+
+        List<List<Integer>> res = new ArrayList<>();
+        for(TreeMap<Integer, PriorityQueue<Integer>> v: map.values()) {
+            List<Integer> temp = new ArrayList<>();
+            for(PriorityQueue<Integer> pq : v.values()) {
+                while(!pq.isEmpty()) temp.add(pq.remove());
             }
-            if (node.right != null) { //right exist karta h toh right jaaoo
-                q.offer(new Tuple(node.right, x + 1, y + 1));
-            }
+            res.add(temp);
         }
-        List < List < Integer >> list = new ArrayList < > ();
-        for (TreeMap < Integer, PriorityQueue < Integer >> ys: map.values()) { //sab vertical par iterate
-            list.add(new ArrayList < > ());   //empty list banai sab vertical ke liye
-            for (PriorityQueue < Integer > nodes: ys.values()) {
-                while (!nodes.isEmpty()) {
-                    list.get(list.size() - 1).add(nodes.poll());
-                }
-            }
-        }
-        return list;
+
+
+        return res;
+    }
+    
+    private void solve(TreeNode root, TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map, int v, int l) {
+        if(root == null) return;
+        
+        map.putIfAbsent(v, new TreeMap<>());
+        map.get(v).putIfAbsent(l, new PriorityQueue<Integer>());
+        
+        map.get(v).get(l).add(root.val);
+        
+        solve(root.left, map, v - 1, l + 1);
+        solve(root.right, map, v + 1, l + 1);
     }
 }
