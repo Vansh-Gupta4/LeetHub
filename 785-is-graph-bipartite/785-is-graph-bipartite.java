6053f0f7b27d8dd0f96de9f9c839600698aa341e
Vansh Gupta <gvansh.411@gmail.com>
2022-05-27 19:00:26 +0530

    Time: 2 ms (51.12%), Space: 53.7 MB (53.71%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,24 +1,35 @@
 class Solution {  //DFS soln
     public boolean isBipartite(int[][] graph) {
         int n = graph.length;
-        int[] colors = new int[n];			
+        int[] colors = new int[n];	
+        for(int i=0;i<colors.length;i++){
+            colors[i]=-1;
+        }
 				
-        for (int i = 0; i < n; i++) {              //This graph might be a disconnected graph. So check each unvisited node.
-            if (colors[i] == 0 && !validColor(graph, colors, 1, i)) {
-                return false;
+        for (int i = 0; i < n; i++) {   //This graph might be a disconnected graph. So check each unvisited node.
+            if (colors[i] == -1) {
+                if(!validColor(graph,i,colors)){
+                    return false;
+                }
             }
         }
         return true;
     }
     
-    public boolean validColor(int[][] graph, int[] colors, int color, int node) {
-        if (colors[node] != 0) {
-            return colors[node] == color;
-        }       
-        colors[node] = color;       
-        for (int next : graph[node]) {
-            if (!validColor(graph, colors, -color, next)) {
-                return false;
+    public boolean validColor(int[][] graph, int node, int[] color) {
+        Queue<Integer> q=new LinkedList<>();
+        q.add(node);
+        color[node]=1;
+        
+        while(!q.isEmpty()){
+            int current=q.poll();
+            for(int c:graph[current]){
+                if(color[c]==-1){
+                    color[c]=1-color[current];
+                    q.add(c);
+                }else if(color[c]==color[current]){
+                    return false;
+                }
             }
         }
         return true;
