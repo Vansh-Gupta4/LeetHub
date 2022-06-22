2d6fde6acb7d8e3562d14d6264c4622beeea57ac
Vansh Gupta <gvansh.411@gmail.com>
2022-06-22 12:05:01 +0530

    Time: 6 ms (58.30%), Space: 45.8 MB (6.52%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -1,36 +1,14 @@
 class Solution {
-    public int findKthLargest(int[] arr, int k) {
-        return quickSelect(arr,0,arr.length - 1,arr.length -k );
-    }
-    public static int quickSelect(int[] arr, int lo, int hi, int k) {
-        int pivot=arr[hi];
-        int pivotIndex=partition(arr,pivot,lo,hi);
-        if (k>pivotIndex){
-            return quickSelect(arr,pivotIndex+1,hi,k);
-        }else if (k<pivotIndex){
-            return quickSelect(arr, lo, pivotIndex-1, k);
-        }else {
-            return arr[pivotIndex];
-        }
-  }
-
-  public static int partition(int[] arr, int pivot, int lo, int hi) {
-    int i = lo, j = lo;
-    while (i <= hi) {
-      if (arr[i] <= pivot) {
-        swap(arr, i, j);
-        i++;
-        j++;
-      } else {
-        i++;
-      }
-    }
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(k + 1);
     
-    return (j - 1);
-  }
-  public static void swap(int[] arr, int i, int j) {
-    int temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
-  }
+        for(int el : nums) {
+            pq.add(el);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+
+        return pq.poll();
+    }
 }
