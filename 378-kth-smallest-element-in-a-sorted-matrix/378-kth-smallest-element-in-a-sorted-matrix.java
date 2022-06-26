24a330a44c9b4c7f929f7170873887faae5b7ec4
Vansh Gupta <gvansh.411@gmail.com>
2022-06-26 18:01:59 +0530

    Time: 24 ms (45.84%), Space: 47.8 MB (89.79%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,32 +1,16 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        int n=matrix.length;
-        int lo=matrix[0][0];
-        int hi=matrix[n-1][n-1];
-        while(lo<hi){
-            int mid=lo+(hi-lo)/2;
-            int count=LessThanEqual(matrix,mid);
-            
-            if(count<k){
-                lo=mid+1;
-            }else{
-                hi=mid;
-            }
+        int m = matrix.length, n = matrix[0].length, ans = -1; // For general, the matrix need not be a square
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>(Comparator.comparingInt(o -> o[0]));
+        for (int r = 0; r < Math.min(m, k); ++r)
+            minHeap.offer(new int[]{matrix[r][0], r, 0});
+
+        for (int i = 1; i <= k; ++i) {
+            int[] top = minHeap.poll();
+            int r = top[1], c = top[2];
+            ans = top[0];
+            if (c + 1 < n) minHeap.offer(new int[]{matrix[r][c + 1], r, c + 1});
         }
-        return lo;
-    }
-    
-    public int LessThanEqual(int[][] matrix, int x){
-        int cnt = 0, N=matrix.length, i=N-1, j=0;
-        //start with bottom left
-        while (i >= 0 && j<N){
-            if(matrix[i][j]>x){
-                i--;
-            }else{
-               cnt = cnt + i + 1;
-               j++;
-            }
-        }    
-        return cnt;
+        return ans;
     }
 }
