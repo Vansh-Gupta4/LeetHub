19dd8fec43d5b6a42338cb56b61d86d74ef5c49a
Vansh Gupta <gvansh.411@gmail.com>
2022-08-05 14:06:05 +0530

    Time: 4 ms (86.98%), Space: 45.4 MB (57.51%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,20 +1,21 @@
 class Solution {
-   public List<List<Integer>> combinationSum(int[] nums, int target) {
-    List<List<Integer>> list = new ArrayList<>();
-    Arrays.sort(nums);
-    backtrack(list, new ArrayList<>(), nums, target, 0);
-    return list;
-}
-
-private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
-    if(remain < 0) return;
-    else if(remain == 0) list.add(new ArrayList<>(tempList));
-    else{ 
-        for(int i = start; i < nums.length; i++){
-            tempList.add(nums[i]);
-            backtrack(list, tempList, nums, remain - nums[i], i); // not i + 1 because we can reuse same elements
-            tempList.remove(tempList.size() - 1);
-        }
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        solve(candidates,0,target,list,new ArrayList<>());
+        return list;
     }
-}
+    public static void solve(int[] arr,int idx,int target, List<List<Integer>> list,List<Integer> tempList){
+        if(idx==arr.length){
+            if(target==0){
+                list.add(new ArrayList <> (tempList));
+            }
+            return;
+        }
+       if(arr[idx] <= target){
+           tempList.add(arr[idx]);
+           solve(arr,idx,target-arr[idx],list,tempList);
+           tempList.remove(tempList.size()-1);
+       }
+        solve(arr,idx+1,target,list,tempList);
+    }
 }
